dc850d753c344d5f3dc9e32059ed7fa9964b26fc
khushi78vaish <158481444+khushi78vaish@users.noreply.github.com>
2024-04-04 00:42:02 +0530

    Add files via upload

---
+++ rowsum.c
@@ -0,0 +1,34 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int n, sum=0;
+    printf("emter the value of n");
+    scanf("%d",&n);
+    int a[n][n];
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    int r;
+    scanf("%d",&r);
+    for(int j=0;j<n;j++)
+    {
+        sum=sum+a[r][j];
+    }
+    printf("%d",sum);
+    
+
+    return 0;
+}
